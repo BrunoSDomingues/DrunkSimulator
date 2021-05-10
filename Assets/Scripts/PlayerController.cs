665a878721da8a396626b09ca0cb9efd3b73e772
Thomas Queiroz <thomasqueirozb@gmail.com>
2021-05-10 20:00:38 -0300

    Procedural generation + fix lighting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     private float baseSpeed = 4f;
     private float gravity = 9.8f;
 
@@ -11,62 +10,52 @@ public class PlayerController : MonoBehaviour
 
     private CharacterController characterController;
 
-    private void Start()
-    {
+    private void Start() {
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
         playerCamera = GameObject.Find("Main Camera");
         cameraRotation = 0.0f;
     }
 
-    private void MovementUpdate()
-    {
+    private void MovementUpdate() {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         float y = 0;
-        if (!characterController.isGrounded)
-        {
+        if (!characterController.isGrounded) {
             y = -gravity;
         }
 
         // Walk to the left
-        if (x < 0)
-        {
+        if (x < 0) {
             animator.SetBool("fWalk", false);
             animator.SetBool("bWalk", false);
             animator.SetBool("lWalk", true);
             animator.SetBool("rWalk", false);
         }
         // Walk to the right
-        else if (x > 0)
-        {
+        else if (x > 0) {
             animator.SetBool("fWalk", false);
             animator.SetBool("bWalk", false);
             animator.SetBool("lWalk", false);
             animator.SetBool("rWalk", true);
-        }
-        else if (x == 0)
-        {
+        } else if (x == 0) {
             // Walk forward
-            if (z > 0)
-            {
+            if (z > 0) {
                 animator.SetBool("fWalk", true);
                 animator.SetBool("bWalk", false);
                 animator.SetBool("lWalk", false);
                 animator.SetBool("rWalk", false);
             }
             // Walk backwards
-            else if (z < 0)
-            {
+            else if (z < 0) {
                 animator.SetBool("fWalk", false);
                 animator.SetBool("bWalk", true);
                 animator.SetBool("lWalk", false);
                 animator.SetBool("rWalk", false);
             }
             // Idle
-            else
-            {
+            else {
                 animator.SetBool("fWalk", false);
                 animator.SetBool("bWalk", false);
                 animator.SetBool("lWalk", false);
@@ -75,24 +64,21 @@ public class PlayerController : MonoBehaviour
         }
 
         Vector3 direction = new Vector3(x, 0, z);
-        
+
         direction = playerCamera.transform.TransformDirection(direction);
         direction = direction * baseSpeed * Time.deltaTime;
         direction.y = y;
 
-        Debug.Log("direction: " + direction);
-
         characterController.Move(direction);
     }
 
-    private void CameraUpdate()
-    {
+    private void CameraUpdate() {
         //Tratando movimentação do mouse
         float mouse_dX = Input.GetAxis("Mouse X");
         float mouse_dY = Input.GetAxis("Mouse Y");
 
         //Tratando a rotação da câmera
-        cameraRotation += mouse_dY;
+        cameraRotation -= mouse_dY;
         Mathf.Clamp(cameraRotation, -75.0f, 75.0f);
 
         transform.Rotate(Vector3.up, mouse_dX);
@@ -100,8 +86,7 @@ public class PlayerController : MonoBehaviour
         playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation, 0.0f, 0.0f);
     }
 
-    private void Update()
-    {
+    private void Update() {
         MovementUpdate();
         CameraUpdate();
     }
---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour {
+    [SerializeField]
+    private GameObject section;
+
+    [SerializeField]
+    private GameObject street;
+
+    void Start() {
+        float z = street.GetComponent<MeshRenderer>().bounds.size.z;
+        float pos = z;
+        for (int i = 0; i < 10; i++) {
+            Instantiate(section, new Vector3(0, 0, pos), Quaternion.identity);
+            pos += z;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
