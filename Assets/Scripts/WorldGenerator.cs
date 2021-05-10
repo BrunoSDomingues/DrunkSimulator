36204354bf0f264a705799c500d0f938b362511a
Thomas Queiroz <thomasqueirozb@gmail.com>
2021-05-10 20:29:23 -0300

    Add house

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour {
     [SerializeField]
@@ -9,17 +7,15 @@ public class WorldGenerator : MonoBehaviour {
     [SerializeField]
     private GameObject street;
 
-    void Start() {
+    void Generate(int roads) {
         float z = street.GetComponent<MeshRenderer>().bounds.size.z;
         float pos = z;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < roads; i++) {
             Instantiate(section, new Vector3(0, 0, pos), Quaternion.identity);
             pos += z;
         }
     }
-
-    // Update is called once per frame
-    void Update() {
-
+    void Start() {
+        Generate(10);
     }
 }
