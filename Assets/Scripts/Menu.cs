5169415330854414b2a8a790d3d3d0af652641d3
Thomas Queiroz <thomasqueirozb@gmail.com>
2021-05-10 22:38:15 -0300

    Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    [SerializeField] GameObject main;
+
+    public void Play() {
+        Debug.Log("Scenou");
+        SceneManager.LoadScene("Game");
+    }
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Startou");
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
