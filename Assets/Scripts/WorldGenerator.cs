c87b03a71d67a65ef4490ff04a047f0cd2636ff7
Thomas Queiroz <thomasqueirozb@gmail.com>
2021-05-10 21:14:17 -0300

    Fix house position instantiation

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -7,15 +7,20 @@ public class WorldGenerator : MonoBehaviour {
     [SerializeField]
     private GameObject street;
 
+    [SerializeField]
+    private GameObject house;
+
+
     void Generate(int roads) {
         float z = street.GetComponent<MeshRenderer>().bounds.size.z;
         float pos = z;
-        for (int i = 0; i < roads; i++) {
+        for (int i = 0; i < roads + 1; i++) {
             Instantiate(section, new Vector3(0, 0, pos), Quaternion.identity);
             pos += z;
         }
+        Instantiate(house, new Vector3(16.6f, 6.2f, pos - z - 1), Quaternion.Euler(0, 180, 0));
     }
     void Start() {
-        Generate(10);
+        Generate(1);
     }
 }
