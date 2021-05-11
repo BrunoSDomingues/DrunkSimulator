dd992b1798df96e57618b79b4344e0d8d2eeb0d2
Thomas Queiroz <thomasqueirozb@gmail.com>
2021-05-10 23:49:38 -0300

    Music

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class Menu : MonoBehaviour {
-    [SerializeField] GameObject main;
+    [SerializeField] AudioSource audiosrc;
+    [SerializeField] Slider slider;
+
 
     public void Play() {
         Debug.Log("Changed to warning");
@@ -10,10 +14,13 @@ public class Menu : MonoBehaviour {
     }
     // Start is called before the first frame update
     void Start() {
+        slider.value = MusicManager.instance.ambienceSource.volume;
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update() { }
 
+    public void VolumeSliderChanged() {
+        MusicManager.instance.ambienceSource.volume = slider.value;
     }
 }
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+// https://stackoverflow.com/questions/27911324/play-continuous-music-when-swapping-between-multiple-scene-in-unity3d
+public class MusicManager : MonoBehaviour {
+    [SerializeField]
+    public AudioSource ambienceSource;
+    [SerializeField]
+    private AudioClip music;
+
+    private static MusicManager _instance;
+
+    public static MusicManager instance {
+
+        get {
+            if (_instance == null) {
+                _instance = GameObject.FindObjectOfType<MusicManager>();
+
+                //Tell unity not to destroy this object when loading a new scene!
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake() {
+        if (_instance == null) {
+            //If I am the first instance, make me the Singleton
+            _instance = this;
+            DontDestroyOnLoad(this);
+            Play();
+        } else {
+            //If a Singleton already exists and you find
+            //another reference in scene, destroy it!
+            if (this != _instance) {
+                Destroy(this.gameObject);
+            } else {
+                Play();
+            }
+        }
+    }
+
+    public void Play() {
+        ambienceSource.loop = true;
+        ambienceSource.clip = music;
+        ambienceSource.Play();
+    }
+}
